3bbe6426ade9f6ed7d89b8ac3e93f326340e954c
Pierre Poulain <pierre.poulain@cupnet.net>
2012-02-23 16:57:19 +0100

    renamed map.R to PBmap.R

--- PBmap.R
+++ PBmap.R
@@ -3,58 +3,56 @@
 # 2012 - P. Poulain, A. G. de Brevern
 
 #====================================================================
-# graphical parameters
-#====================================================================
-# name of output file:
-name = "show.pdf"
-
-# define graphical parameters
-definePar = function() {
-par(
-    # default margins are: 5.1 4.1 4.1 2.1
-    # extend bottom margin for text (+5 line)
-    mar = c(5.1, 5.1, 4.1, 2.1),
-    oma = c(2,0,0,0), # 2 lines for comments: 0 to 4
-    lwd=3,          # line width
-    bty = 'o',      # type of box around graphic
-    font.lab = 2,   # axis label font (bold)
-    font.axis = 2,  # axis font (bold)
-    cex.lab=1.7,    # axis label width
-    cex.axis=1.5    # axis width
-)}
-
-# print filename and date on top
-printNameDate = function() {
-mtext( paste(name, Sys.Date()), side = 3, line = 3)
-}
-
-#====================================================================
 # load data
 #====================================================================
 args = commandArgs(TRUE)
 filename = args[1]
+lower = as.integer(args[2])
+upper = as.integer(args[3])
+
 data = read.table(filename, header = TRUE, row.names = 1)
 
+if (length(colnames(data)) != 16) {
+    cat("Bad format. 16 + 1 columns expected.\nBye bye\n")
+    quit()
+}
+
+# lower / upper bound
+if (is.na(lower)) lower = min(as.numeric(rownames(data)))
+if (is.na(upper)) upper = max(as.numeric(rownames(data)))
+
 # select data range
-#map = data[27:37,]
-map = data
+map = data[lower:upper,]
 
 # define row and col names
 xnames = rownames(map)
 ynames = colnames(map)
 
-total = sum(map[3,])
+# get total number of counts
+total_count = sum(map[3,])
 # convert 0 to NA
 map[map==0] = NA
-# normalize by the number of points
-map = map / total
+# normalize by the total number of counts
+map = map / total_count
+
 
 #====================================================================
-# start output file
+# graphical parameters
 #====================================================================
-png(filename="map.png", width = log(length(xnames))*250, height = 800)
-definePar()
-
+name="PBmap.png"
+png(filename=name, width = log(length(xnames))*250, height = 800)
+par(
+    # default margins are: 5.1 4.1 4.1 2.1
+    # extend bottom margin for text (+5 line)
+    mar = c(5.1, 5.1, 4.1, 2.1),
+    oma = c(2,0,0,0), # 2 lines for comments: 0 to 4
+    lwd=3,            # line width
+    bty = 'o',        # type of box around graphic
+    font.lab = 2,     # axis label font (bold)
+    font.axis = 2,    # axis font (bold)
+    cex.lab=1.7,      # axis label width
+    cex.axis=1.5      # axis width
+)
 colorpal = rev(heat.colors(10))
 #====================================================================
 # plot
@@ -65,12 +63,13 @@ box()
 axis(1, seq(0, 1, 1/(length(xnames)-1)), xnames)
 axis(2, seq(0, 1, 1/(length(ynames)-1)), ynames, font = 4)
 
+mtext(paste(name, Sys.Date()), side = 3, line = 3)
 
-
+cat(paste("wrote", name, "\n"))
 #====================================================================
 # close output file
 #====================================================================
-toto = dev.off()
+void = dev.off()
 # avoid the ouput of "null device"
 
 
