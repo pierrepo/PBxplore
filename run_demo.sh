9f627de82622ee7e74038f093f3d00750e47796d
Pierre Poulain <pierre.poulain@cupnet.net>
2012-10-31 09:10:54 +0100

    run_demo.sh: added pause() function and final file listing

--- run_demo.sh
+++ run_demo.sh
@@ -2,13 +2,23 @@
 
 # 2012 - P. Poulain, A. G. de Brevern
 
+function pause(){
+    read -s -n1 -p "Press any key to continue."
+    echo
+#read -r -n1 -p "Press any key to continue."
+# wait 5 sec from key pressed and then goes on
+#read -t5 -s -n1 -p "Press any key to continue."
+
+}
+
+
 echo "#------------------------------------------------------------------------#"
 echo "|                                                                        |"
 echo "|  demo script for PBxplore                                              |"
 echo "|                                                                        |"
 echo "#------------------------------------------------------------------------#"
 
-read -n1 -r -p "Press any key to continue."
+pause
 
 # create and move into the demo directory
 mkdir demo
@@ -24,21 +34,21 @@ echo "#------------------------------------------------------------------------#
 echo  -e "\n"
 echo "Test with 3ICH.pdb: RX structure, one chain"
 echo "../PBassign.py -f 3ICH.pdb -o 3ICH"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p 3ICH.pdb -o 3ICH
 
 
 echo  -e "\n"
 echo "Test with 1AY7.pdb: RX structure, complex with two chains"
 echo "../PBassign.py -f 1AY7.pdb -o 1AY7"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p 1AY7.pdb -o 1AY7
 
 
 echo  -e "\n"
 echo "Test with 2LFU.pdb: RMN structure, 10 models"
 echo "../PBassign.py -f 2LFU.pdb -o 2LFU"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p 2LFU.pdb -o 2LFU
 
 
@@ -53,7 +63,7 @@ echo "#------------------------------------------------------------------------#
 echo  -e "\n"
 echo "Test with 3ICH.pdb: RX structure, one chain"
 echo "../PBassign.py -f 3ICH.pdb -o 3ICH --phipsi"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p 3ICH.pdb -o 3ICH --phipsi
 
 
@@ -68,7 +78,7 @@ echo "#------------------------------------------------------------------------#
 echo  -e "\n"
 echo "Test with 2LFU.pdb: RMN structure, 10 models"
 echo "../PBassign.py -f 2LFU.pdb -o 2LFU --flat"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p 2LFU.pdb -o 2LFU --flat
 
 
@@ -82,13 +92,23 @@ echo "#------------------------------------------------------------------------#
 echo  -e "\n"
 echo "Test with several PDB files"
 echo "../PBassign.py -d ./ -o all"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p 3ICH.pdb -p 2LFU.pdb -o several
 
 echo  -e "\n"
 echo "Test with all PDB files from a directory"
 echo "../PBassign.py -d ./ -o all"
-read -n1 -r -p "Press any key to continue."
+pause
 ../PBassign.py -p ./ -o all
 
+
+echo  -e "\n"
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Demo completed!                                                       |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+echo 
+echo "Look at *.PB.* files in the demo directory."
+ls -lh 
 
