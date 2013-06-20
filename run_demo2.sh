af97d6a93fbf206a01054ba2250e133ee7dc7671
Pierre Poulain <pierre.poulain@cupnet.net>
2013-06-20 23:49:43 +0100

    wrote a demo script for multiple conformation analysis

---
+++ run_demo2.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+
+# 2013 - P. Poulain, A. G. de Brevern
+
+# exit script at first error
+set -e
+
+function pause(){
+    read -r -s -n1 -p "Press any key to continue."
+    echo
+}
+
+
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Demo script for PBxplore: multiple conformation analysis              |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+
+pause
+
+# create and move into the demo directory
+mkdir demo2_tmp
+cd demo2_tmp
+cp ../demo2/* ./
+
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Protein Blocks assignment: many conformations of the same protein    |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+
+echo  -e "\n"
+echo "Test with the 225 conformations of the beta3 integrin"
+echo "../PBassign.py -p md_traj.pdb -o md_traj"
+pause
+../PBassign.py -p md_traj.pdb -o md_traj
+
+
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Make statistics (Neq)                                                 |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+
+echo  -e "\n"
+echo "../PBstat.py -f md_traj.PB.fasta -o md_traj"
+pause
+../PBstat.py -f md_traj.PB.fasta -o md_traj
+
+
+echo  -e "\n"
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Make statistics with several PB assignment fasta files ption         |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+
+
+echo  -e "\n"
+echo "Test with several 'PB.fasta' files"
+echo "../PBstat.py -f md_traj_1.PB.fasta -f md_traj_2.PB.fasta -f md_traj_3.PB.fasta -o test_output"
+pause
+../PBstat.py -f md_traj_1.PB.fasta -f md_traj_2.PB.fasta -f md_traj_3.PB.fasta -o test_output
+
+
+echo  -e "\n"
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Making statitics: playin with Neq graphic                             |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+
+
+echo  -e "\n"
+echo "Test with --neq-residue-lower, --neq-residue-upper, and --neq-residue-shift options"
+echo "../PBstat.py -f md_traj.PB.fasta -o md_traj --neq-residue-lower=10 --neq-residue-upper=30 --neq-residue-shift=5"
+pause
+../PBstat.py -f md_traj.PB.fasta -o md_traj --neq-residue-lower=10 --neq-residue-upper=30 --neq-residue-shift=5
+
+
+echo  -e "\n"
+echo "#------------------------------------------------------------------------#"
+echo "|                                                                        |"
+echo "|  Demo completed!                                                       |"
+echo "|                                                                        |"
+echo "#------------------------------------------------------------------------#"
+echo 
+echo "Look at *.PB.* files in the demo directory."
+pwd
+ls -lh 
+
