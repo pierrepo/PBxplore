0d598f2789afcd10cb563118408377ee59cfd3b5
Jonathan Barnoud <jonathan@barnoud.net>
2015-05-02 10:09:57 +0200

    Small PEP8 improvements in PBassign

--- PBassign.py
+++ PBassign.py
@@ -4,7 +4,7 @@
 """
 Read PDB structures and assign protein blocs (PBs).
 
-2013 - P. Poulain, A. G. de Brevern 
+2013 - P. Poulain, A. G. de Brevern
 """
 
 
@@ -15,7 +15,7 @@ from __future__ import print_function
 import os
 import sys
 import glob
-import argparse 
+import argparse
 
 ## local module
 import PBlib as PB
@@ -53,18 +53,14 @@ def PB_assign(pb_ref, structure, comment,
               fasta_file, flat_file=_NullContext(), phipsi_file=_NullContext()):
     """assign Protein Blocks (PB) from phi and psi angles
     """
-    # get phi and psi angles from structure
     dihedrals = structure.get_phi_psi_angles()
-    #print(dihedrals)
+    pb_seq = PB.assign(dihedrals, pb_ref)
+
+    # write PBs in fasta file
+    PB.write_fasta_entry(fasta_file, pb_seq, comment)
     # write phi and psi angles
     if not isinstance(phipsi_file, _NullContext):
         PB.write_phipsi_entry(phipsi_file, dihedrals, comment)
-
-    pb_seq = PB.assign(dihedrals, pb_ref)
-    
-    # write PBs in fasta file
-    PB.write_fasta_entry(fasta_file, pb_seq, comment)
-    
     # write PBs in flat file
     if not isinstance(flat_file, _NullContext):
         print(pb_seq, file=flat_file)
@@ -74,26 +70,28 @@ def PB_assign(pb_ref, structure, comment,
 
 def user_inputs():
     parser = argparse.ArgumentParser(
-        description = 'Read PDB structures and assign protein blocs (PBs).')
+        description='Read PDB structures and assign protein blocs (PBs).')
 
     # arguments
     parser.add_argument("-p", action="append",
-        help="name of a pdb file or name of a directory containing pdb files")
-    parser.add_argument("-o", action="store", required = True,
-        help="name for results")
+                        help=("name of a pdb file "
+                              "or name of a directory containing pdb files")
+    parser.add_argument("-o", action="store", required=True,
+                        help="name for results")
     # arguments for MDanalysis
     group = parser.add_argument_group(
         title='other options [if MDanalysis module is available]')
-    group.add_argument("-x", action="store", 
-        help="name of xtc file (Gromacs)")
-    group.add_argument("-g", action="store", 
-        help="name of gro file (Gromacs)")
+    group.add_argument("-x", action="store",
+                       help="name of xtc file (Gromacs)")
+    group.add_argument("-g", action="store",
+                       help="name of gro file (Gromacs)")
     # optional arguments
     parser.add_argument("--phipsi", action="store_true", default=False,
-        help="writes phi and psi angle")
+                        help="writes phi and psi angle")
     parser.add_argument("--flat", action="store_true", default=False,
-        help="writes one PBs sequence per line")
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+                        help="writes one PBs sequence per line")
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s 1.0')
     # get all arguments
     options = parser.parse_args()
 
@@ -122,7 +120,7 @@ def user_inputs():
             # input is not a file neither a directory: say it
             elif (not os.path.isfile(name) or not os.path.isdir(name)):
                 parser.error("{0}: not a valid file or directory".format(name))
-    else:   
+    else:
         if not os.path.isfile(options.x):
             sys.exit("{0}: not a valid file".format(options.x))
         elif not os.path.isfile(options.g):
@@ -134,7 +132,7 @@ def user_inputs():
 def pbassign_cli():
     options, pdb_name_lst = user_inputs()
 
-    if options.p :
+    if options.p:
         if pdb_name_lst:
             print("{} PDB file(s) to process".format(len(pdb_name_lst)))
         else:
@@ -161,18 +159,18 @@ def pbassign_cli():
     else:
         # PB assignement of a Gromacs trajectory
         chains = PDB.chains_from_trajectory(options.x, options.g)
-    
+
     with fasta_file, phipsi_file, flat_file:
         for comment, chain in chains:
                 PB_assign(PB.REFERENCES, chain, comment,
                           fasta_file=fasta_file, flat_file=flat_file,
                           phipsi_file=phipsi_file)
 
-    print( "wrote {0}".format(fasta_name) )
+    print("wrote {0}".format(fasta_name))
     if options.flat:
-        print( "wrote {0}".format(flat_name) )
+        print("wrote {0}".format(flat_name))
     if options.phipsi:
-        print( "wrote {0}".format(phipsi_name) )
+        print("wrote {0}".format(phipsi_name))
 
 if __name__ == '__main__':
     pbassign_cli()
