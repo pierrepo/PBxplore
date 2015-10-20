10379c2210b39d507af61530c56c1dbfa8cf5307
Jonathan Barnoud <jonathan@barnoud.net>
2015-10-20 16:45:24 +0200

    Add a function to list absolute path to demo files

--- pbxplore/demo/__init__.py
+++ pbxplore/demo/__init__.py
@@ -10,9 +10,12 @@ constant can be accessed as :const:`pbxplore.demo.DEMO_DATA_PATH`, or as
 :const:`pbxplore.DEMO_DATA_PATH`.
 
 A list of the available demonstration files is available with the
-:func:`list_demo_files` function.
+:func:`list_demo_files` function. The same list with absolute path instead of
+file names is provided by :func:`list_demo_files_absolute`.
 
 .. autofunction:: pbxplore.demo.list_demo_files
+
+.. autofunction:: pbxplore.demo.list_demo_files_absolute
 """
 
 import os
@@ -23,5 +26,19 @@ DEMO_DATA_PATH=os.path.abspath(os.path.dirname(__file__))
 def list_demo_files():
     """
     List the names of the bundled demo files
+
+    File names starting with _ or . are not listed. This allows to omit
+    __init__.py, and hiden files.
     """
-    return os.listdir(DEMO_DATA_PATH)
+    return [demo_file for demo_file in os.listdir(DEMO_DATA_PATH)
+            if not demo_file[0] in '_.']
+
+def list_demo_files_absolute():
+    """
+    List the absolute path to the bundled demo files
+
+    File names starting with _ or . are not listed. This allows to omit
+    __init__.py, and hiden files.
+    """
+    return [os.path.join(DEMO_DATA_PATH, demo_file) for demo_file
+            in list_demo_files()]
