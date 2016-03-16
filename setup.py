9f16415eeca826c722960a97396c9fa933ff129f
Pierre Poulain <pierre.poulain@cupnet.net>
2016-03-16 17:45:18 +0100

    Change README extension

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import pbxplore
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
 
 
