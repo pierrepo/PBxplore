42b44489da189aa5e3799f8069dd47d0a638bf7a
Pierre Poulain <pierre.poulain@cupnet.net>
2016-12-21 16:45:53 +0100

    Remove support for old Python versions (2.6 and 3.3)

--- setup.py
+++ setup.py
@@ -46,12 +46,10 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 
     install_requires=['numpy', 'MDAnalysis>=0.11', 'matplotlib'],
