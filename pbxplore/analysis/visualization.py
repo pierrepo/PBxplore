bb53f4205bfdace89673c84be6ecb814715d3ff4
gabrielctn <gabrielcretin@gmail.com>
2021-01-26 12:01:34 +0100

    One more revert

--- pbxplore/analysis/visualization.py
+++ pbxplore/analysis/visualization.py
@@ -162,7 +162,7 @@ def plot_map(fname, count_mat, idx_first_residue=1, residue_min=1, residue_max=N
     # add ticks and labels
     ax2.set_xticks(xticks - numpy.min(xticks))
     ax2.set_xticklabels(xticks)
-    ax2.set_yticks(list(range(len(yticks))))
+    ax2.set_yticks(range(len(yticks)))
     ax2.set_yticklabels(yticks, style='italic', weight='bold')
     ax2.set_xlabel("Residue number", weight="bold")
 
